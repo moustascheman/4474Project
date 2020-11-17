9f17626a645c2e1e9785f9c0ea3e13f57f8b72cc
Rahman Ahmed <rahmanrafa@hotmail.com>
2020-11-16 19:25:54 -0500

    Aiming Arrow and Basic Firing
    Added a 2nd aiming arrow that is a sprite rather than a slider. It has a basic move script attached that lets users move it (translate) by dragging it. Also added in basic firing to the tank. It currently fires a placeholder shell in an arc.

--- Assets/Scripts/Tank/Tank.cs
+++ Assets/Scripts/Tank/Tank.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Basic Tank Script that handles basic tank functions that can be unified between both modes
+//Should be extended with inheritance for MP mode
 public class Tank : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    //Projectiles should fire from this point
+
+    public List<GameObject> Inventory;
+    public GameObject firingPoint;
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class Weapon : ScriptableObject
+{
+    public string displayName;
+    public string description;
+
+    public Sprite projectileSprite;
+
+    public float weight;
+    public float damage;
+
+}
