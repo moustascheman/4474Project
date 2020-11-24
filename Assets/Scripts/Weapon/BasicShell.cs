51e3273b56ac6dcea39ed4dc7bb5af6e555cc37d
Rahman Ahmed <rahmanrafa@hotmail.com>
2020-11-24 14:29:19 -0500

    Health and Turn System
    Added Basic Turn System. Added health and damage to tanks. Win state currently does not work as there needs to be a delay added after firing for all calculations to be finished.

---
+++ Assets/Scripts/Weapon/BasicShell.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicShell : Projectile
+{
+
+    public float base_damage = 20;
+    public float explosion_rad;
+
+    public override void OnCollisionEnter2D(Collision2D col)
+    {
+
+        base.OnCollisionEnter2D(col);
+    }
+
+    public override void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.layer == LayerMask.NameToLayer("Hitbox"))
+        {
+            col.gameObject.GetComponentInParent<TankHealth>().dealDamage(base_damage);
+        }
+
+        base.OnTriggerEnter2D(col);
+    }
+
+
+}
