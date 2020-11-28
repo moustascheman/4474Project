a3bcf24fa47341a5cb8f3f75e6a50faae9a0244a
Rahman Ahmed <rahmanrafa@hotmail.com>
2020-11-27 21:45:41 -0500

    Explosion AoE Damage
    Added AoE damage to explosion for both direct hits and misses for the basic shell prefab projectile

--- Assets/Scripts/Weapon/BasicShell.cs
+++ Assets/Scripts/Weapon/BasicShell.cs
@@ -11,11 +11,14 @@ public class BasicShell : Projectile
 
     public override void OnCollisionEnter2D(Collision2D col)
     {
-        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, explosion_rad, hitboxMask);
+        Collider2D[] cols = Physics2D.OverlapCircleAll(col.transform.position, explosion_rad, hitboxMask);
         foreach(Collider2D tankCol in cols)
         {
             
             TankHealth health = tankCol.gameObject.GetComponentInParent<TankHealth>();
+            float dam = calculateDamage(col.transform.position, tankCol.transform.position);
+            health.dealDamage(dam);
+            Debug.Log("Dealt " + dam + " damage");
 
         }
         base.OnCollisionEnter2D(col);
@@ -27,14 +30,30 @@ public class BasicShell : Projectile
         {
             col.gameObject.GetComponentInParent<TankHealth>().dealDamage(base_damage);
         }
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, explosion_rad, hitboxMask);
+        foreach(Collider2D tankCol in cols)
+        {
+            if(tankCol != col)
+            {
+                TankHealth health = tankCol.gameObject.GetComponentInParent<TankHealth>();
+                float dam = calculateDamage(col.transform.position, tankCol.transform.position);
+                health.dealDamage(dam);
+                Debug.Log("Dealt " + dam + " damage");
+            }
+        }
 
         base.OnTriggerEnter2D(col);
     }
 
-    private void calculateDamage(Vector3 sourcePos, Vector3 destPos)
+    private float calculateDamage(Vector3 sourcePos, Vector3 destPos)
     {
 
-
+        float dist = Vector2.Distance(sourcePos, destPos);
+        Debug.Log("DISTANCE " + dist);
+        float relativeDist = (explosion_rad - dist) / explosion_rad;
+        float dam = base_damage * relativeDist;
+        dam = Mathf.Max(0f, dam);
+        return dam;
     }
 
 }
