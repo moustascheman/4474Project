a10d57d76daa15cafc250efcda6c9a59d5907ec5
Zachary Tan <ztan52@uwo.ca>
2020-12-01 02:55:35 -0500

    change direction of tank depending on mouse relative to tank (be sure to add game manager to each tank)

--- Assets/Scripts/Tank/AimingArrow.cs
+++ Assets/Scripts/Tank/AimingArrow.cs
@@ -11,15 +11,31 @@ public class AimingArrow : MonoBehaviour
     public float minAngleValue = -110;
     public float maxAngleValue = 110;
     public float angle = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+
+    private Vector3 playerScreenPoint;
+    private Tank currentTank;
+    private Transform tower; // <= sphere
+    public MultiplayerGameManager gameManager;
     // Update is called once per frame
     void Update()
     {
-        
+        //get the current turn's tank
+        currentTank = gameManager.getCurrentPlayerTank();
+
+        //access the tower(sphere) of the tank
+        tower = currentTank.transform.GetChild(1).gameObject.transform;
+
+        // convert the coordinate of the player to the screen and then work out if the mouse is left or right of the player
+        playerScreenPoint = Camera.main.WorldToScreenPoint(currentTank.transform.position);
+
+        //rotate tank depending where mouse is relative to tank object
+        if(Input.mousePosition.x <= playerScreenPoint.x) {
+            //set the rotate to the tower(sphere) to face right
+            tower.transform.eulerAngles  = new Vector3(tower.transform.eulerAngles.x, 180, tower.transform.eulerAngles.z);
+        } else {
+            //set the rotate to the tower(sphere) to face left
+            tower.transform.eulerAngles = new Vector3(tower.transform.eulerAngles.x, 0, tower.transform.eulerAngles.z);
+        }
     }
 }
