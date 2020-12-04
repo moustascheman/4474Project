3d7296da8acddee6c002ce5bedd165d572036259
Rahman Ahmed <rahmanrafa@hotmail.com>
2020-12-04 10:56:04 -0500

    Basic Shop Screen setup
    Basic shop screen setup

---
+++ Assets/Scripts/GameManagers/ShopManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShopManager : MonoBehaviour
+{
+    public MultiplayerGameManager gm;
+    public TextMeshProUGUI currentMoneyText;
+
+    // prices
+    public int largeShellPrice = 30;
+    public int railGunPrice = 50;
+    
+
+    
+}
---
+++ Assets/Scripts/UI/ShopEntry.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+/*
+ * Class used for each entry in the shop
+ * The class should only control UI elements related to each entry and does not affect player variables such
+ * as money and ammo
+ */
+public class ShopEntry : MonoBehaviour
+{
+    public Button buyButton;
+    public TextMeshProUGUI fundsMessage;
+
+    public void disableEntry()
+    {
+        buyButton.interactable = false;
+        fundsMessage.text = "Not enough money";
+    }
+
+    public void enableEntry()
+    {
+        buyButton.interactable = true;
+        fundsMessage.text = "";
+    }
+
+
+}
