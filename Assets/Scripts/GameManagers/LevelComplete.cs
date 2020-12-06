267cc6713d658908a3476ebb95b3229712da4299
Zachary Tan <ztan52@uwo.ca>
2020-12-06 14:06:43 -0500

    added a winner anim that transitions to main menu

---
+++ Assets/Scripts/GameManagers/LevelComplete.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+}
