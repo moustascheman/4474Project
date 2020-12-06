a983eebf9084a37c818e2373e756f7491459479a
Samer Alabi <sameralabi@hotmail.com>
2020-12-05 22:13:06 -0500

    Add Explosion Effects

---
+++ Assets/Scripts/Weapon/DestroyExplosionOnExit.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyExplosionOnExit : StateMachineBehaviour {
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {   
+        Destroy(animator.transform.parent.gameObject, stateInfo.length);
+    }
+}
