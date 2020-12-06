0b9653490f1bd70bee7ef9973ad7ba4641facaf8
initdev <rkenne49@uwo.ca>
2020-12-06 03:20:52 -0500

    Created a nicer looking main menu

---
+++ Assets/Scripts/UI/MenuButtonManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonManager : MonoBehaviour
+{
+    public void exitBtn()
+    {
+        Application.Quit();
+    }
+}
