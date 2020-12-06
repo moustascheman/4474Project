3056fb51704fd567c0e5c7cc617ce925269f2acf
initdev <rkenne49@uwo.ca>
2020-12-06 12:35:45 -0500

    Changed resolution

--- Assets/Scripts/UI/MenuButtonManager.cs
+++ Assets/Scripts/UI/MenuButtonManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Not done yet, will need for button behaviors
 public class MenuButtonManager : MonoBehaviour
 {
     public void exitBtn()
